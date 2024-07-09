5070fffbfa3f7a5eb506dbc63526f5bbbda491e8
Aliansyah <aliansyaahian@gmail.com>
2024-07-09 17:45:44 +0700

    Membuat method update untuk update data Post ke DB

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Resources\PostResource;
 use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Validator;
 
 class PostController extends Controller
@@ -73,5 +74,55 @@ class PostController extends Controller
 
         // Return single post as a resource
         return new PostResource(true, 'Detail data post.', $post);
+    }
+
+    /**
+     * update
+     *
+     * @param  mixed $request
+     * @param  mixed $id
+     * @return void
+     */
+    public function update(Request $request, $id)
+    {
+        // Define validation rules
+        $validator = Validator::make($request->all(), [
+            'title' => 'required',
+            'content' => 'required',
+        ]);
+
+        // Check if validation fails
+        if ($validator->fails()) {
+            // Error validation
+            return response()->json($validator->errors(), 422);
+        }
+
+        // Find post by ID
+        $post = Post::find($id);
+
+        if ($request->hasFile('image')) {
+            // Upload image
+            $image = $request->file('image');
+            $image->storeAs('public/posts', $image->hashName());
+
+            // Delete old image
+            Storage::delete('public/posts/'.basename($post->image));
+
+            // Update post with new image
+            $post->update([
+                'image' => $image->hashName(),
+                'title' => $request->title,
+                'content' => $request->content,
+            ]);
+
+        } else {
+            // Update post without image
+            $post->update([
+                'title' => $request->title,
+                'content' => $request->content,
+            ]);
+        }
+        
+        return new PostResource(true, 'Data post berhasil diubah.', $post);
     }
 }
