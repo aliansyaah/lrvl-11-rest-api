e4379796b9fa3e3050b298723af978b221c1bb0a
Aliansyah <aliansyaahian@gmail.com>
2024-07-09 17:23:07 +0700

    Membuat method store untuk insert data Post ke DB

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Resources\PostResource;
 use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class PostController extends Controller
 {
@@ -21,5 +22,41 @@ class PostController extends Controller
 
         // Return collection of post as a resource
         return new PostResource(true, 'List data post', $posts);
+    }
+
+    /**
+     * store
+     *
+     * @param  mixed $request
+     * @return void
+     */
+    public function store(Request $request)
+    {
+        // Define validation rules
+        $validator = Validator::make($request->all(), [
+            'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+            'title' => 'required',
+            'content' => 'required',
+        ]);
+
+        // Check if validation fails
+        if ($validator->fails()) {
+            // Error validation
+            return response()->json($validator->errors(), 422);
+        }
+
+        // Upload image
+        $image = $request->file('image');
+        $image->storeAs('public/posts', $image->hashName());
+        /* File gambar yang diupload akan dimasukan ke dalam folder "storage", yang berada di dalam "storage/app/public/posts" */
+
+        // Create post
+        $post = Post::create([
+            'image' => $image->hashName(),
+            'title' => $request->title,
+            'content' => $request->content,
+        ]);
+
+        return new PostResource(true, 'Data post berhasil ditambahkan.', $post);
     }
 }
