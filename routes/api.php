a0a7620542d7f9205294a1f865997630894083a0
Aliansyah <aliansyaahian@gmail.com>
2024-07-10 18:29:30 +0700

    Membuat CRUD product menggunakan auth sanctum

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,7 @@ Route::controller(RegisterController::class)->group(function(){
     Route::post('register', 'register');
     Route::post('login', 'login');
 });
+
+Route::middleware('auth:sanctum')->group(function(){
+    Route::resource('products', ProductController::class);
+});
